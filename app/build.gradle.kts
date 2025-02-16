c89f797b77fa5764a7a7ce057aafab6acf3e919d
Kirilligu <k.degtyarev4@mail.ru>
2025-02-16 18:38:48 +0800

    AlertDialog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
 
 dependencies {
     implementation ("com.google.code.gson:gson:2.12.1")
+    implementation ("com.google.android.material:material:1.9.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -19,36 +19,63 @@ class MainActivity : AppCompatActivity() {
     private val briefWeatherFragment = BriefWeatherFragment()
     private val detailedWeatherFragment = DetailedWeatherFragment()
     private lateinit var citySpinner: Spinner
+    private lateinit var themeSpinner: Spinner
     private var selectedCity = "Irkutsk"
+    private var isDarkTheme = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         fm = supportFragmentManager
         citySpinner = findViewById(R.id.citySpinner)
+        themeSpinner = findViewById(R.id.themeSpinner)
         val cities = listOf("Irkutsk", "Sochi", "Moscow", "Novosibirsk", "Habarovsk")
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, cities)
-        citySpinner.adapter = adapter
-        if (fm.findFragmentById(R.id.container_fragm) == null) {
-            fm.beginTransaction().add(R.id.container_fragm, briefWeatherFragment).commit()
-        }
-        findViewById<Button>(R.id.btnBrief).setOnClickListener {
-            loadWeather()
-            fm.beginTransaction().replace(R.id.container_fragm, briefWeatherFragment).commit()
-        }
-
-        findViewById<Button>(R.id.btnDetailed).setOnClickListener {
-            loadWeather()
-            fm.beginTransaction().replace(R.id.container_fragm, detailedWeatherFragment).commit()
-        }
+        val themes = listOf("Темная тема", "Светлая тема")
+        citySpinner.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, cities)
+        themeSpinner.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, themes)
         citySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parentView: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                 selectedCity = citySpinner.selectedItem.toString()
                 loadWeather()
             }
-            override fun onNothingSelected(parentView: AdapterView<*>?) {
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+        themeSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                isDarkTheme = position == 0
+                applyTheme()
             }
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+        applyTheme()
+        findViewById<Button>(R.id.btnBrief).setOnClickListener {
+            fm.beginTransaction().replace(R.id.container_fragm, briefWeatherFragment).commit()
+            loadWeather()
+        }
+        findViewById<Button>(R.id.btnDetailed).setOnClickListener {
+            fm.beginTransaction().replace(R.id.container_fragm, detailedWeatherFragment).commit()
+            loadWeather()
+        }
+        if (fm.findFragmentById(R.id.container_fragm) == null) {
+            fm.beginTransaction().add(R.id.container_fragm, briefWeatherFragment).commit()
         }
         loadWeather()
+    }
+
+    private fun applyTheme() {
+        val rootLayout = findViewById<View>(R.id.rootLayout)
+        val btnBrief = findViewById<Button>(R.id.btnBrief)
+        val btnDetailed = findViewById<Button>(R.id.btnDetailed)
+
+        if (isDarkTheme) {
+            rootLayout.setBackgroundResource(R.color.dark_theme_background)
+            btnBrief.setBackgroundResource(R.color.dark_theme_button)
+            btnDetailed.setBackgroundResource(R.color.dark_theme_button)
+        } else {
+            rootLayout.setBackgroundResource(R.color.light_theme_background)
+            btnBrief.setBackgroundResource(R.color.light_theme_button)
+            btnDetailed.setBackgroundResource(R.color.light_theme_button)
+        }
+
     }
     private fun loadWeather() {
         lifecycleScope.launch(Dispatchers.IO) {
@@ -65,12 +92,17 @@ class MainActivity : AppCompatActivity() {
     private fun fetchWeatherData(): WeatherResponse {
         val API_KEY = "4dac63af9f28277443bf2d581d6cc50b"
         val weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=$selectedCity&appid=$API_KEY&units=metric&lang=ru"
+
         val stream = URL(weatherURL).openStream()
         val reader = InputStreamReader(stream)
         return Gson().fromJson(reader, WeatherResponse::class.java)
     }
     private fun updateFragments(weatherData: WeatherResponse) {
-        if (briefWeatherFragment.isAdded) briefWeatherFragment.updateWeather(weatherData)
-        if (detailedWeatherFragment.isAdded) detailedWeatherFragment.updateWeather(weatherData)
+        if (briefWeatherFragment.isAdded) {
+            briefWeatherFragment.updateWeather(weatherData)
+        }
+        if (detailedWeatherFragment.isAdded) {
+            detailedWeatherFragment.updateWeather(weatherData)
+        }
     }
 }
